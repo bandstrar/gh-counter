02e1a9740347e702fd9b8b226f08ffab4a2aec82
bandstrar <rob.bandstra@gmail.com>
2020-08-06 20:20:22 -0500

    added ability to add more players and not put score below 0

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 'use strict';
 
-let counterValue = 0;
+const playerArray = [
+    {
+    name: 'Player',
+    score: 0
+},
+]
 
 const buildCounter = () => {
-  let domString = `<div class="counter--card">
-                        <h3 class="counter--header">Counter</h3>
-                        <div class="counter--value">${counterValue}</div>
+  let domString =  ''
+  
+
+  for (let i = 0; i < playerArray.length; i++)
+  domString +=      `<div class="counter--card">
+                        <h3 class="counter--header">${playerArray[i].name} ${i + 1}</h3>
+                        <div class="counter--value score--${i}">${playerArray[i].score}</div>
                         <div class="counter--buttons">
-                            <button id="decrease">Decrease</button>
-                            <button id="reset">Reset</button>
-                            <button id="increase">Increase</button>
+                            <button id="decrease--${i}">Decrease</button>
+                            <button id="reset--${i}">Reset</button>
+                            <button id="increase--${i}">Increase</button>
                         </div>
                     </div>`;
 
@@ -22,34 +31,46 @@ const printToDom = (divId, textToPrint) => {
 };
 
 const changeColor = () => {
-    if (counterValue === 0) {
-        document.querySelector('.counter--value').style.color = '#DAA588';
-    } else if (counterValue < 0) {
-        document.querySelector('.counter--value').style.color = '#F56960';
-    } else if (counterValue > 0) {
-        document.querySelector('.counter--value').style.color = '#9DCBBA';
+
+    for (let i = 0; i < playerArray.length; i++) {
+        if (playerArray[i].score === 0) {
+            document.querySelector(`.score--${i}`).style.color = '#DAA588';
+        } else if (playerArray[i].score < 0) {
+            document.querySelector(`.score--${i}`).style.color = '#F56960';
+        } else if (playerArray[i].score > 0) {
+            document.querySelector(`.score--${i}`).style.color = '#9DCBBA';
+        }
     }
 }
 
-const decreaseCounter = () => {
-    counterValue -= 1;
+
+const changeScore = (e) => {
+    const target = e.target.id;
+
+    for (let i = 0; i < playerArray.length; i++) {
+        if (target === `decrease--${i}` && playerArray[i].score > 0) {
+            playerArray[i].score -= 1;
+        } else if (target === `increase--${i}`) {
+            playerArray[i].score += 1;
+        } else if (target === `reset--${i}`) {
+            playerArray[i].score = 0;
+        }
+    }
     init();
 }
 
-const increaseCounter = () => {
-    counterValue += 1;
-    init();
-}
-
-const resetCounter = () => {
-    counterValue = 0;
+const addPlayer = () => {
+    playerArray.push({name: 'Player', score: 0})
     init();
 }
 
 const buttonEvent = () => {
-    document.querySelector('#decrease').addEventListener('click', decreaseCounter);
-    document.querySelector('#increase').addEventListener('click', increaseCounter);
-    document.querySelector('#reset').addEventListener('click', resetCounter);
+    for (let i = 0; i < playerArray.length; i++) {
+    document.querySelector(`#decrease--${i}`).addEventListener('click', changeScore);
+    document.querySelector(`#increase--${i}`).addEventListener('click', changeScore);
+    document.querySelector(`#reset--${i}`).addEventListener('click', changeScore);
+    }
+    document.querySelector('#addButton').addEventListener('click', addPlayer)
 }
 
 const init = () => {
